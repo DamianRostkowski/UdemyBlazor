9e44087009da39e1a51ea619b5d3adb4616eb00b
Damian Rostkowski <damian.rostkowski@gmail.com>
2024-06-17 21:33:08 +0200

    150 vidijo

---
+++ Tangy_Models/OrderHeaderDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class OrderHeaderDTO
+    {
+        public int Id { get; set; }
+        [Required] 
+        public string UserId { get; set; }
+        [Required]
+        [Display(Name ="Order Total")]
+        public double OrderTotal {  get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [Display(Name ="Shipping Date")]
+        public DateTime ShippingDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+        [Display(Name ="Name")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Phone Number")]
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Street Address")]
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [Display(Name="Postal Code")]
+        public string PostalCode { get; set; }
+        [Display(Name ="Email")]
+        [Required]
+        public string Email { get; set; }
+    }
+}
